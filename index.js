8718344ea0b94dbe491d2ad172c1d5ea273b42e3
Ben Koska <ben@koska.at>
2018-08-06 00:52:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,184 +1,61 @@
 import React, { Component } from 'react';
 import {
     StyleSheet,
+    TextInput,
     View
 } from 'react-native';
 import {
     func,
     number,
     object,
-    string,
 } from 'prop-types';
-import axios, { CancelToken } from 'axios';
-import AutocompleteInput from './lib/AutocompleteInput';
-import Predictions from './lib/Predictions';
+import debounce from 'lodash.debounce';
 
-class GooglePlaceAutocomplete extends Component {
+class AutocompleteInput extends Component {
     static propTypes = {
-        googleAPIKey: string,
-        value: string,
+        custom: object,
         debounce: number,
-        style: object,
-        customStyle: object,
-        inputStyle: object,
-        predictionsStyle: object,
-        placeholder: string,
-        placeholderTextColor: string,
-        underlineColorAndroid: string,
-        onChangeText: func,
-        onPredictions: func,
-        onResult: func
+        onChangeText: func.isRequired,
+        onChangeTextSettle: func.isRequired
     }
-
-    static defaultProps = {
-        debounce: 250,
-        containerStyle: {},
-        inputStyle: {},
-        predictionsStyle: {},
-    }
-
-    _cancelTokenSource;
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            value: ''
-        };
-
-        this._cancelTokenSource = CancelToken.source();
-    }
-
-    componentDidMount() {
-        this.setState({
-            value: this.props.value
-        });
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.value !== this.props.value) {
-            this.setState({
-                value: nextProps.value
-            });
-        }
     }
 
     render() {
+        const {custom = {}, ...props} = this.props;
+
         return (
-            <View style={[style.container, this.props.style]}>
-                <AutocompleteInput
-                    value={this.state.value}
-                    inputStyle={this.props.inputStyle}
-                    containerStyle={this.props.containerStyle}
-                    placeholder={this.props.placeholder}
+            <View style={[style.container, custom.container]}>
+                <TextInput
+                    {...props}
                     onChangeText={this._handleChangeText}
-                    selectionColor={this.props.selectionColor}
-                    underlineColorAndroid={this.props.underlineColorAndroid}
-                    placeholderTextColor={this.props.placeholderTextColor}
-                    debounce={this.props.debounce}
-                    onChangeTextSettle={this._handleChangeTextSettle} />
-                <Predictions
-                    predictionsStyle={this.props.predictionsStyle}
-                    predictions={this.state.predictions}
-                    onPressPrediction={this._handlePressPrediction} />
+                    style={[style.input, custom.input]} />
             </View>
         );
     }
 
     _handleChangeText = (value) => {
-        this.setState({value});
-
-        // Fire event
-        if (this.props.onChangeText) {
-            this.props.onChangeText(value);
-        }
+        this.props.onChangeText(value);
+        this._settle();
     }
 
-    _handleChangeTextSettle = () => {
-        if (this.state.value.length > 0) {
-            // Get predictions
-            this._getPredictions(this.state.value)
-            .then(response => {
-                if (response && response.data) {
-                       this._predictions(response.data.features);
-                }
-            })
-            .catch(error => {
-                console.log('error', error);
-            });
-        } else {
-            if (this.state.predictions && this.state.predictions.length > 0) {
-                this._predictions([]);
-            }
-        }
-    }
-
-    _predictions(predictions) {
-        this.setState({predictions});
-
-        // Fire event
-        if (this.props.onPredictions) {
-            this.props.onPredictions(predictions);
-        }
-    }
-
-    _handlePressPrediction = (prediction) => {
-        console.log(prediction);
-        this.props.onResult(prediction);
-//         // Get more detail about the place
-//         this._request('/place/details', {
-//             placeid: prediction.place_id
-//         })
-//         .then(response => {
-//             if (response && response.data) {
-//                 if (response.data.status === 'OK') {
-//                     const {result} = response.data;
-
-//                     // Fire event
-//                     if (this.props.onResult) {
-//                         this.setState({
-//                           predictions:[],
-//                         });
-//                         this.props.onResult(result);
-//                     }
-//                 } else {
-//                     console.error('Request Error:', response.data.error_message || response.data.status);
-//                 }
-//             }
-//         })
-//         .catch(error => {
-//             console.log('error', error);
-//         });
-    }
-
-    _getPredictions = (input) => {
-        // Cancel any other requests
-        this._cancelTokenSource.cancel('The "clean slate" protocol');
-        this._cancelTokenSource = CancelToken.source();
-
-        var params = {}
-        params.q = input
-        params.limit = 5
-
-        return axios({
-            url: `api`,
-            method: 'get',
-            baseURL: 'http://photon.komoot.de',
-            params,
-            cancelToken: this._cancelTokenSource.token
-        })
-        .catch(error => {
-            if (axios.isCancel(error)) {
-                console.log('Request Cancel:', error.message);
-            } else {
-                throw error;
-            }
-        });
-    }
+    _settle = debounce(() => {
+        this.props.onChangeTextSettle();
+    }, this.props.debounce);
 }
 
 export const style = StyleSheet.create({
-    container: {}
+    container: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 44,
+    },
+
+    input: {
+        height: '100%',
+    }
 });
 
-export default GooglePlaceAutocomplete;
+export default AutocompleteInput;
